1e2a8237a2571c1a1cfb7e417e05dbc0579fef67
nenni01 <nikolay.nenchev@gmail.com>
2012-11-07 21:31:55 +0200

    3

--- src/com/hp/opencall/ngin/nmsim/NmSimCallUser.java
+++ src/com/hp/opencall/ngin/nmsim/NmSimCallUser.java
@@ -5,7 +5,6 @@ import javax.sql.DataSource;
 import org.apache.log4j.Logger;
 
 import com.hp.opencall.ngin.scif.Address;
-import com.hp.opencall.ngin.scif.AddressableContact;
 import com.hp.opencall.ngin.scif.Call;
 import com.hp.opencall.ngin.scif.CallLeg;
 import com.hp.opencall.ngin.scif.CallUser;
@@ -13,15 +12,13 @@ import com.hp.opencall.ngin.scif.Cause;
 import com.hp.opencall.ngin.scif.Contact;
 import com.hp.opencall.ngin.scif.PollEvent;
 import com.hp.opencall.ngin.scif.ScifException;
-import com.hp.opencall.ngin.scif.TerminalContact;
 import com.hp.opencall.ngin.scif.parameters.CommonParameterSet;
+import com.hp.opencall.ngin.scif.parameters.CsCallParameterSet;
 
 public class NmSimCallUser implements CallUser {
 
 	private static Logger _log = Logger.getLogger(NmSimCallUser.class);
 	private Call aCall = null;
-	private Address aInitialNumber = null;
-	private Address bInitialNumber = null;
 	private DataSource datasource = null;
 
 	public NmSimCallUser(Call aCall, DataSource datasource) {
@@ -64,39 +61,45 @@ public class NmSimCallUser implements CallUser {
 		_log.debug("ID - getId: " + aCall.getId());
 		_log.debug("CDR: " + aCall.getCdr());
 
-		aInitialNumber = leg.getLegAddress();
-
-		if (contact instanceof AddressableContact) {
-			AddressableContact addressablecontact = (AddressableContact) contact;
-			bInitialNumber = addressablecontact.getAddress();
-		}
-		_log.debug("aInitialNumber: " + aInitialNumber);
-		_log.debug("bInitialNumber: " + bInitialNumber);
-
 		switch (cause) {
 		case NONE:
 			try {
 
-				CommonParameterSet commonpSet = aCall
-						.getParameterSet(CommonParameterSet.class);
-				_log.debug("Call Attempt time: "
-						+ commonpSet.getCallAttempTime());
+				CommonParameterSet commonpSet = aCall.getParameterSet(CommonParameterSet.class);
+				_log.debug("Call Attempt time: " + commonpSet.getCallAttempTime());
 				_log.debug("Call Service Key: " + commonpSet.getServiceKey());
-				_log.debug("Call Network Call Type: "
-						+ commonpSet.getNetworkCallType());
-				_log.debug("Call Switching Point: "
-						+ commonpSet.getSwitchingPoint());
-
-				//commonpSet.setCallingParty();
-				 
-				commonpSet.setCallingParty(NmSimMONewCgPA.getNewCgPA(datasource, leg));
+				_log.debug("Call Network Call Type: " + commonpSet.getNetworkCallType());
+				_log.debug("Call Switching Point: " + commonpSet.getSwitchingPoint());
 				
-				Contact newbcontact = new TerminalContact(
-						"tel:359878010230;network-context=TS24008.1.1");
-				_log.debug("newbcontact: " + newbcontact);
-				aCall.leaveCall(Cause.DONE, newbcontact);
-				// aCall.continueCall();
-
+				CsCallParameterSet cspSet = aCall.getParameterSet(CsCallParameterSet.class);
+				_log.debug("CgPA IMSI: " + cspSet.getIMSI());
+				_log.debug("MSC address:" + cspSet.getMscAddress().getUri());
+				_log.debug("Location number" + cspSet.getLocationNumber().getUri());
+				_log.debug("Localtion Information:" + cspSet.getLocationInformation());
+				_log.debug("Trigger event type: " + cspSet.getEventTypeBCSM());
+				
+				if(commonpSet.getNetworkCallType().toString().equals("ORIGINATING")){
+				 
+				 Address newCgPA = NmSimMONewCgPA.getNewCgPA(datasource, leg);
+				 _log.debug("newCgPA: " + newCgPA.getUri());
+					if(!newCgPA.getUser().equals(leg.getLegAddress().getUser())){
+						_log.debug("Set new calling party address(as camel generic number): " + newCgPA.getUri());
+						commonpSet.setCallingParty(newCgPA);
+					}
+				
+				aCall.continueCall();
+				}
+				else if (commonpSet.getNetworkCallType().toString().equals("TERMINATING")) {
+					_log.warn("Call type is:" + commonpSet.getNetworkCallType().toString());
+					aCall.forwardCall(contact);
+					
+				}
+				else{
+					_log.warn("Call type is not Originating, Forwarding or Terminating");
+					_log.warn("Call type is:" + commonpSet.getNetworkCallType().toString());
+				aCall.abortCall(Cause.INTERNAL);	
+				}
+				
 			} catch (ScifException e) {
 				_log.error("Cannot continue the call", e);
 			}
